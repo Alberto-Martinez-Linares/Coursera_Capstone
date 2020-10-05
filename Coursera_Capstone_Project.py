a2bb01d3542b65d74a2f3ce616583719d0f9b1c8
Alberto-Martinez-Linares <62058264+Alberto-Martinez-Linares@users.noreply.github.com>
2020-10-05 14:49:59 +0200

    Add files via upload

---
+++ Coursera_Capstone_Project.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Captsone Project
+# This notebook will be used fot the Captsone project for the IBM Data Science Certificate. In this project I will be working on a case study which is to predict the severity of an accident. The data set for this exercise will be from the Seatle city.
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print ('Hello Capstone Project Course!')
+
